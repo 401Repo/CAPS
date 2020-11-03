ad18281d4356cbd42724caf1564dd40cc71b6aca
thatvetdevrob <thatvetdevrob@gmail.com>
2020-11-03 18:53:18 -0500

    swapped for sockets at port 5000

--- events.js
+++ events.js
@@ -1,6 +1,13 @@
+
+
 'use strict';
 
 // the events for all modules
+// for phase two what can i do here?....
+const io = require('socket.io-client');
+let serverHost = 'http://localhost:5000';
+const hostConnection = io.connect(serverHost);
+
 
 const Events = require('events');
 const events = new Events();
